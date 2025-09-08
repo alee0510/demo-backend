9e89b59d732fafdde8f724f7e01be93c73d20821
Ali Muksin <ali.muksin0510@gmail.com>
2025-09-08 09:33:47 +0700

    chore: update config

--- api/index.js
+++ api/index.js
@@ -3,8 +3,12 @@ import { createServerlessExpress } from "@vendia/serverless-express";
 
 const app = express();
 
-app.get("/api/hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.json({ message: "Hello from Express on Vercel!" });
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API!" });
 });
 
 // Export as Vercel handler
